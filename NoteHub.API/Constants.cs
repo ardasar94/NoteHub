dab9dd795399f827469dc7ec9fe5a793feb7f90d
ardasar94 <sarihanarda@gmail.com>
2021-05-24 12:18:11 +0300

    JWT auth

--- NoteHub.API/Constants.cs
+++ NoteHub.API/Constants.cs
@@ -9,6 +9,7 @@ namespace NoteHub.API
     {
         public const string DEFAULT_EMAIL = "admin@example.com";
         public const string DEFAULT_PASSWORD = "P@ssword1";
+        public const string AUTH_SIGNING_KEY = "yA24*FASD₺d2*kaf__342L$``d*f*k3a??-2134ad,*/--asfd";
 
         public static class Roles
         {
